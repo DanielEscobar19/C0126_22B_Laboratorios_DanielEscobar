e55de254f73b1f2b16a084949c6464e545fbb76f
DanielEscobar19 <danielonchio@gmail.com>
2022-09-01 14:18:19 -0600

    Se crea cancion favorita y se retorna

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,30 @@
+﻿// Daniel Escobar Giraldo | C02748
+
+using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = this.FavoriteSong();
+            ViewBag.MainTitle = "My favorite song:";
+            return View(song);
+        }
+
+        private SongModel FavoriteSong()
+        {
+            return new SongModel
+            {
+                Id = 1,
+                Name = "Take it",
+                Artist = "Dom Dolla",
+                Genre = "Techno",
+                ReleaseDate = new DateTime(2018, 7, 27),
+                Duration = new TimeSpan(0, 3, 54)
+            };
+        }
+    }
+}
