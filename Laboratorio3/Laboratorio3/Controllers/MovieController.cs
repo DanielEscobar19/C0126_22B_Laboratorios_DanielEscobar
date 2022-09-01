6e2a2bb4936b92d82c5441c657842b3ac94739c2
DanielEscobar19 <danielonchio@gmail.com>
2022-09-01 12:36:02 -0600

    Controlador devuelve una lista de peliculas

--- Laboratorio3/Laboratorio3/Controllers/MovieController.cs
+++ Laboratorio3/Laboratorio3/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿// Daniel Escobar Giraldo | C02748
 
+using Laboratorio3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Laboratorio3.Controllers
@@ -8,7 +9,35 @@ namespace Laboratorio3.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var movies = GetListOfMovies();
+            ViewBag.MainTitle = "These are my favorite movies";
+            return View(movies);
+        }
+        private List<MovieModel> GetListOfMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(new MovieModel
+            {
+                Id = 1,
+                Name = "Pulp Fiction",
+                Genre = "Crime/Drama",
+                ReleaseDate = new DateTime(1994, 10, 14)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 2,
+                Name = "Toy Store",
+                Genre = "Family/Comedy",
+                ReleaseDate = new DateTime(1995, 11, 22)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 3,
+                Name = "Mulan",
+                Genre = "Family/Comedy",
+                ReleaseDate = new DateTime(1998, 06, 19)
+            });
+            return movies;
         }
     }
 }
