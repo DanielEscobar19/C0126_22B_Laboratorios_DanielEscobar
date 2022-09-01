17c78a747107f7b7336dd0a29fe1672c9e96ab40
DanielEscobar19 <danielonchio@gmail.com>
2022-09-01 14:18:34 -0600

    Cancion almacena 5 atributos de una cancion

---
+++ Laboratorio3/Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,15 @@
+﻿// Daniel Escobar Giraldo | C02748
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan Duration { get; set; }
+
+    }
+}
