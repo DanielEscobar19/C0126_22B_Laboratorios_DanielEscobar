849b15fb953cc826ace4db27f1fe25634dca598a
DanielEscobar19 <danielonchio@gmail.com>
2022-09-12 14:11:20 -0600

    Handler maenja datos entre modelo y base de datos

---
+++ Laboratorio5/Handlers/PeliculasHandler.cs
@@ -0,0 +1,53 @@
+﻿/*
+    Daniel Escobar Giraldo | C02748
+*/
+using System;
+using System.Collections.Generic;
+using Laboratorio5.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+namespace Laboratorio5.Handlers
+{
+    public class PeliculasHandler
+    {
+        private SqlConnection conexion;
+        private string rutaConexion;
+        public PeliculasHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion = builder.Configuration.GetConnectionString("ContextoDePeliculas");
+            conexion = new SqlConnection(rutaConexion);
+        }
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta,
+            conexion);
+            SqlDataAdapter adaptadorParaTabla = new
+            SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+        public List<PeliculaModel> ObtenerPeliculas()
+        {
+            List<PeliculaModel> peliculas = new List<PeliculaModel>();
+            string consulta = "SELECT * FROM Pelicula";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                peliculas.Add(
+                new PeliculaModel
+                {
+                    ID = Convert.ToInt32(columna["Id"]),
+                    Nombre = Convert.ToString(columna["Nombre"]),
+                    Año = Convert.ToInt32(columna["Año"]),
+                });
+            }
+            return peliculas;
+        }
+    }
+}
