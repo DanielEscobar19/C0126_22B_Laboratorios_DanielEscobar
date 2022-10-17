16baf5138e1e748d2c10af59c6c8b7b0090f8934
DanielEscobar19 <danielonchio@gmail.com>
2022-10-17 11:29:02 -0600

    Copia del proyecto del laboratorio 5 a carpeta del lab 6

---
+++ Laboratorio6/Laboratorio5/Controllers/PeliculasController.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Daniel Escobar Giraldo | C02748
+ */
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Laboratorio5.Handlers;
+
+namespace Laboratorio5.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        public IActionResult Index()
+        {
+            PeliculasHandler peliculasHandler = new PeliculasHandler();
+            var peliculas = peliculasHandler.ObtenerPeliculas();
+            ViewBag.MainTitle = "Lista de Peliculas";
+            return View(peliculas);
+        }
+    }
+}
---
+++ Laboratorio6/Laboratorio5/Models/PeliculaModel.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Daniel Escobar Giraldo | C02748 
+*/
+
+namespace Laboratorio5.Models
+{
+    public class PeliculaModel
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Año { get; set; }
+    }
+}
