cbdb0c46ccf1bddf70f9b4089109f3e6c7de4032
DanielEscobar19 <danielonchio@gmail.com>
2022-12-05 18:37:29 -0600

    Se crea scaffolding del controlador del modelo Company. Esto agrega vistas de borrar, editar y agregar.

---
+++ Examen2/app_source/Data/CompanyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using app_source.Models;
+
+namespace app_source.Data
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext (DbContextOptions<CompanyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<app_source.Models.CompanyModel> CompanyModel { get; set; } = default!;
+    }
+}
--- Examen2/app_source/Program.cs
+++ Examen2/app_source/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using app_source.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CompanyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CompanyContext") ?? throw new InvalidOperationException("Connection string 'CompanyContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Companies}/{action=Index}/{id?}");
+    pattern: "{controller=Company}/{action=Index}/{id?}");
 
 app.Run();
