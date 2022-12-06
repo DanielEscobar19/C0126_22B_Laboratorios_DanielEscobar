34a401fd1df151d85fab58b9cfd3d8d2370e57f7
DanielEscobar19 <danielonchio@gmail.com>
2022-12-05 19:12:05 -0600

    Atributo ValorEstiamdo se asigana nullable para que no sea required en el form.

--- Examen2/app_source/Models/CompanyModel.cs
+++ Examen2/app_source/Models/CompanyModel.cs
@@ -30,7 +30,8 @@ namespace app_source.Models
         // valor estimado total de la empresa
         // puede ser un numero real
         [DisplayName("Valor estimado de la empresa:")]
-        public decimal ValorEstimado { get; set; }
+        [DefaultValue(0)]
+        public decimal? ValorEstimado { get; set; }
 
         // true si es transacional y false en caso contrario
         [Required(ErrorMessage = "Debe indicar si la empresa es trasnacional")]
