60ecd557acb5a4b42c9b3fd4503f5a5485fdfb9c
DanielEscobar19 <danielonchio@gmail.com>
2022-12-05 20:15:41 -0600

    Ya no so required el tipo de negocio y el pais base

--- Examen2/app_source/Models/CompanyModel.cs
+++ Examen2/app_source/Models/CompanyModel.cs
@@ -14,26 +14,22 @@ namespace app_source.Models
 
         // el tipo de negocio es muy variado
         // puede ser distribuidor, contruccion, restaurante, etc.
-        [Required(ErrorMessage = "Debe indicar el tipo de empresa")]
         [StringLength(256, ErrorMessage = "El tipo de negocio debe tener menos de 256 carácteres")]
         [DisplayName("Tipo de negocio:")]
-        [DefaultValue("Desconocido")]
         public string? TipoNegocio { get; set; }
 
         // pais donde esta la sede principal de la empresa
-        [Required(ErrorMessage = "Debe indicar el país de origen de la empresa")]
         [StringLength(256, ErrorMessage = "El nombre del país base debe tener menos de 256 carácteres")]
         [DisplayName("País base de la empresa:")]
-        [DefaultValue("Desconocido")]
         public string? PaisBase { get; set; }
 
         // valor estimado total de la empresa
         // puede ser un numero real
         [DisplayName("Valor estimado de la empresa:")]
-        [DefaultValue(0)]
         public decimal? ValorEstimado { get; set; }
 
         // true si es transacional y false en caso contrario
+        [DisplayName("¿La empresa es trasnacional?")]
         [Required(ErrorMessage = "Debe indicar si la empresa es trasnacional")]
         public bool EsTransnacional { get; set; }
     }
