7a1a696a9650349901795f4a623593461b85574a
DanielEscobar19 <danielonchio@gmail.com>
2022-12-07 21:01:57 -0600

    Se quita el dos puntos en cada display de los atributos

--- Examen2/app_source/Models/CompanyModel.cs
+++ Examen2/app_source/Models/CompanyModel.cs
@@ -14,24 +14,24 @@ namespace app_source.Models
         // nombre de la empresa
         [Required(ErrorMessage = "Debe indicar el nombre de la empresa")]
         [StringLength(256, ErrorMessage = "El nombre de la empresa debe tener menos de 256 carácteres")]
-        [DisplayName("Nombre de la empresa:")]
+        [DisplayName("Nombre de la empresa")]
         [Remote("IsCompanyNameAvailable", "Company", AdditionalFields = nameof(Id), HttpMethod = "POST", ErrorMessage = "Ya existe una empresa con este nombre")]
         public string? Nombre { get; set; }
 
         // el tipo de negocio es muy variado
         // puede ser distribuidor, contruccion, restaurante, etc.
         [StringLength(256, ErrorMessage = "El tipo de negocio debe tener menos de 256 carácteres")]
-        [DisplayName("Tipo de negocio:")]
+        [DisplayName("Tipo de negocio")]
         public string? TipoNegocio { get; set; }
 
         // pais donde esta la sede principal de la empresa
         [StringLength(256, ErrorMessage = "El nombre del país base debe tener menos de 256 carácteres")]
-        [DisplayName("País base de la empresa:")]
+        [DisplayName("País base de la empresa")]
         public string? PaisBase { get; set; }
 
         // valor estimado total de la empresa
         // puede ser un numero real
-        [DisplayName("Valor estimado de la empresa:")]
+        [DisplayName("Valor estimado de la empresa")]
         public decimal? ValorEstimado { get; set; }
 
         // true si es transacional y false en caso contrario
