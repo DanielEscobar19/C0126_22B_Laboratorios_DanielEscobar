91c5490876261bbcf50c256618cb0350c6b77af1
DanielEscobar19 <danielonchio@gmail.com>
2022-12-08 20:58:38 -0600

    Se crea proyecto de Nunit para reemplazar el de Xunit por limitaciones con los setup y los teardown.

---
+++ Examen2/unit_tests/Helper.cs
@@ -0,0 +1,27 @@
+﻿using app_source.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_tests
+{
+    public static class Helper
+    {
+        private static string Connectionstring = "Data Source=172.16.202.209;Initial Catalog=C02748;TrustServerCertificate=True;Persist Security Info=True;User ID=C02748;Password=c02748";
+
+        public static CompanyContext DbContext
+        {
+            get
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<CompanyContext>();
+                optionsBuilder.UseSqlServer(Connectionstring);
+
+                return new CompanyContext(optionsBuilder.Options);
+            }
+
+        }
+    }
+}
