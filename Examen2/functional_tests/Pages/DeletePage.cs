04876367fc8b127be1c3685d997cb272c9889811
DanielEscobar19 <danielonchio@gmail.com>
2022-12-08 20:45:36 -0600

    Clase que maneja inpust en la vetana de eliminar empresa.

---
+++ Examen2/functional_tests/Pages/DeletePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional_tests.Pages
+{
+    public class DeletePage
+    {
+        private IWebDriver Driver;
+        public DeletePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebElement BotonEliminar
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("botonEliminar"), Driver);
+            }
+        }
+    }
+}
