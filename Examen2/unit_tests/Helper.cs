8c26430463d12afc8b521c6b9edd59cb10864a91
DanielEscobar19 <danielonchio@gmail.com>
2022-12-08 22:04:36 -0600

    Clase de ayuda. Se agrega metodo y atributo para manejar base de datos.

--- Examen2/unit_tests/Helper.cs
+++ Examen2/unit_tests/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using app_source.Data;
+using app_source.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ namespace unit_tests
     public static class Helper
     {
         private static string Connectionstring = "Data Source=172.16.202.209;Initial Catalog=C02748;TrustServerCertificate=True;Persist Security Info=True;User ID=C02748;Password=c02748";
+
+        public const string UNIQUEID = "FXc21c02748";
 
         public static CompanyContext DbContext
         {
