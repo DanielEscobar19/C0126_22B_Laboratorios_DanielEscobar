c41b75bebc908273b4aec8c5fbbe0913a5ce09f6
DanielEscobar19 <danielonchio@gmail.com>
2022-12-09 13:26:04 -0600

    Documentacion del helper.

--- Examen2/functional_tests/Helper.cs
+++ Examen2/functional_tests/Helper.cs
@@ -10,12 +10,24 @@ using app_source.Data;
 
 namespace functional_tests
 {
+    /// <summary>
+    /// Clase con metodos que asisten en los test
+    /// </summary>
     public class Helper
     {
+        /// <summary>
+        /// string con la url a la que acceder en los test
+        /// </summary>
         public static string URL = "https://localhost:7013/";
 
+        /// <summary>
+        /// connection string con la base de datos que se desea usar
+        /// </summary>
         private static string Connectionstring = "Data Source=172.16.202.209;Initial Catalog=C02748;TrustServerCertificate=True;Persist Security Info=True;User ID=C02748;Password=c02748";
 
+        /// <summary>
+        /// instancia que permite acceder y manejar la base de datos
+        /// </summary>
         public static CompanyContext DbContext
         {
             get
@@ -28,6 +40,12 @@ namespace functional_tests
 
         }
 
+        /// <summary>
+        /// metodo que busca un elemento y hace una espera de 20 segundos
+        /// </summary>
+        /// <remark>
+        /// se usa para evitar errores si la pagina dura en cargar y el elemento no se muestra a tiempo
+        /// </remark>
         static public IWebElement FindWaitElement(By identificador, IWebDriver driver)
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
