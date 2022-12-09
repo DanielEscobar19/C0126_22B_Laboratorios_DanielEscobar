38226ed87b3741fe61d017d07d51b64233fd316a
DanielEscobar19 <danielonchio@gmail.com>
2022-12-08 23:12:01 -0600

    Se agrega constante para hacer nombres unicos que no choquen con datos en la base.

--- Examen2/unit_tests/Helper.cs
+++ Examen2/unit_tests/Helper.cs
@@ -13,8 +13,6 @@ namespace unit_tests
     {
         private static string Connectionstring = "Data Source=172.16.202.209;Initial Catalog=C02748;TrustServerCertificate=True;Persist Security Info=True;User ID=C02748;Password=c02748";
 
-        public const string UNIQUEID = "FXc21c02748";
-
         public static CompanyContext DbContext
         {
             get
@@ -25,6 +23,32 @@ namespace unit_tests
                 return new CompanyContext(optionsBuilder.Options);
             }
 
+        }
+
+        public static void InsertarEmpresasSemilla(ref List<CompanyModel> EmpresasSemilla, CompanyContext dbContext, string uniqueId)
+        {
+            // ingresamos empresas de prueba
+            // creamos lista con empresas de prueba
+            for (int i = 0; i < 4; ++i)
+            {
+                CompanyModel tempCompany = new CompanyModel
+                {
+                    // solo asignamos un nombre porque nos interesa probrar si se peude repetir nombres
+                    Nombre = $"Testing {i} {uniqueId}",
+                    TipoNegocio = $"Negocio de testing {i} {uniqueId}",
+                    PaisBase = $"Pais de testing {i} {uniqueId}",
+                    ValorEstimado = 45678.56m,
+                    EsTransnacional = true
+                };
+                // ingresamos en la base y y actualizamos el modelo actual
+                // la actualizacion es para recibir el numero de id que asigna la base
+                tempCompany = dbContext.Add(tempCompany).Entity;
+
+                EmpresasSemilla.Add(tempCompany);
+            }
+
+            // guardamos en la base los cambios realizados
+            dbContext.SaveChanges();
         }
     }
 }
