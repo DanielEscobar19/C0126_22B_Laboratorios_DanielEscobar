ea00eda4ee0d85cc941336016f3b9e1b882d0f90
DanielEscobar19 <danielonchio@gmail.com>
2022-12-08 20:45:03 -0600

    Clase para manejar los inputs de las paginas de actualizar y de crear.

---
+++ Examen2/functional_tests/Pages/CompanyInputs.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional_tests.Pages
+{
+    public class CompanyInputs
+    {
+        private IWebDriver Driver;
+        public CompanyInputs(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebElement BotonAceptar
+        {
+            get {
+                return Helper.FindWaitElement(By.Name("botonAceptar"), Driver);
+            }
+        }
+
+        public IWebElement BotonCancelar
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Name("botonCancelar"), Driver);
+            }
+        }
+
+        public IWebElement InputNombre
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("inputNombre"), Driver);
+            }
+        }
+        public IWebElement InputTipo
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("inputTipo"), Driver);
+            }
+        }
+
+        public IWebElement InputPais
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("inputPais"), Driver);
+            }
+        }
+
+        public IWebElement InputValor
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("inputValor"), Driver);
+            }
+        }
+
+        public IWebElement InputEsTransnacional
+        {
+            get
+            {
+                return Helper.FindWaitElement(By.Id("inputEsTransnacional"), Driver);
+            }
+        }
+    }
+}
